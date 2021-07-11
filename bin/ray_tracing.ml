528fb3a7eabcb107520f9ac95b509ffc6fe3f6b8
Tianyi Song <42670338+tysg@users.noreply.github.com>
2021-07-12 01:24:49 +0800

    Diffusion model

--- bin/ray_tracing.ml
+++ bin/ray_tracing.ml
@@ -13,46 +13,31 @@ let hit_sphere (center : Vec3.t) (radius : float) (ray : Ray.t) : float option =
   if delta >= 0. then Some ((~-.half_b -. sqrt delta) /. a) else None
 
 
-let hit_world world ray =
-  match Sphere.hit_list ray 0. Float.max_float world with
-  | Some hit_rec ->
-      (hit_rec.normal +/ 1.) */ 0.5
-  | None ->
-      let unit_direction = unit_vector ray.direction in
-      let t = 0.5 *. (unit_direction.y +. 1.0) in
-      gradient Color.white Color.light_blue t
-
-
-(* image *)
-let aspect_ratio = 16. /. 9.
-
-(* camera *)
-let viewport_height = 2.0
-
-let viewport_width = aspect_ratio *. viewport_height
-
-let focal_length = 1.0
-
-let origin = Vec3.zero
-
-let horizontal = Vec3.create viewport_width 0. 0.
-
-let vertical = Vec3.create 0. viewport_height 0.
-
-let lower_left_corner =
-  origin
-  -| (horizontal // 2.)
-  -| (vertical // 2.)
-  -| Vec3.create 0. 0. focal_length
-
-
-let render_ray u v =
-  Ray.create
-    origin
-    (lower_left_corner +| (horizontal */ u) +| (vertical */ v) -| origin)
+let rec ray_color world ray depth_limit =
+  if depth_limit <= 0
+  then Vec3.zero
+  else
+    match Sphere.hit_list ray 0. Float.max_float world with
+    | Some hit_rec ->
+        let target =
+          hit_rec.point +| hit_rec.normal +| Vec3.random_in_unit_sphere ()
+        in
+        ray_color
+          world
+          (Ray.create hit_rec.point (target -| hit_rec.point))
+          (depth_limit - 1)
+        */ 0.5
+    | None ->
+        let unit_direction = unit_vector ray.direction in
+        let t = 0.5 *. (unit_direction.y +. 1.0) in
+        gradient Color.white Color.light_blue t
 
 
 let render =
+  let () = Random.self_init () in
+  let samples_per_pixel = 100 in
+  let camera = Camera.create Camera.viewport in
+  let aspect_ratio = 16. /. 9. in
   let image_width = 400 in
   let image_height = Int.of_float (Float.of_int image_width /. aspect_ratio) in
   print_string
@@ -63,17 +48,30 @@ let render =
     ^ " "
     ^ "\n255\n" ) ;
   let world =
-    [ 
-     Sphere.create (Vec3.create 0. (-100.5) (-1.)) 100.;
-      Sphere.create (Vec3.create 0. 0. (-1.)) 0.5;
+    [ Sphere.create (Vec3.create 0. (-100.5) (-1.)) 100.
+    ; Sphere.create (Vec3.create 0. 0. (-1.)) 0.5
     ]
   in
   for j = image_height - 1 downto 0 do
-    Printf.eprintf "\rScanlines remaining: %d\n" j ;
+    Printf.eprintf "Scanlines remaining: %d\n" j ;
+    flush stderr ;
     for i = 0 to image_width - 1 do
-      let to_frac i total = Float.of_int i /. Float.of_int (total - 1) in
-      let u = to_frac i image_width in
-      let v = to_frac j image_height in
-      render_ray u v |> hit_world world |> Color.write_color |> print_string
+      let to_frac i total =
+        (Float.of_int i +. Math.random_frac ()) /. Float.of_int (total - 1)
+      in
+      let r = ref 0. in
+      let g = ref 0. in
+      let b = ref 0. in
+
+      for _ = 0 to samples_per_pixel - 1 do
+        let u = to_frac i image_width in
+        let v = to_frac j image_height in
+        let ray = Camera.get_ray camera u v in
+        let color = ray_color world ray 50 in
+        r := !r +. color.x ;
+        g := !g +. color.y ;
+        b := !b +. color.z
+      done ;
+      Color.write_color (Vec3.create !r !g !b) samples_per_pixel |> print_string
     done
   done
